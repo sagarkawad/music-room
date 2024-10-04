7b81b977611a01b2d3db3a4edb64bfd858d385a2
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2024-10-04 23:31:04 +0530

    yt seek functionality added

--- components/YouTubeVideo.tsx
+++ components/YouTubeVideo.tsx
@@ -2,22 +2,34 @@
 
 import React from "react";
 import YouTube, { YouTubeEvent, YouTubeProps } from "react-youtube";
+import { useRef } from "react";
 
 // Define the type for props, including sendMessage
 interface YouTubeVideoProps {
   sendMessage: (time: number) => void;
 }
 
+// Define the YouTube Player Interface
+interface YouTubePlayer {
+  seekTo(seconds: number, allowSeekAhead: boolean): void;
+}
+
 const YouTubeVideo: React.FC<YouTubeVideoProps> = ({ sendMessage }) => {
+  const playerRef = useRef<YouTubePlayer | null>(null);
+
   const onPlayerReady = (event: YouTubeEvent) => {
     // Access the player object and call methods if needed
     event.target.pauseVideo();
     console.log(event.target);
+    playerRef.current = event.target;
   };
 
   const onStateChange = (event: YouTubeEvent) => {
     console.log(event.target.playerInfo.currentTime);
     sendMessage(event.target.playerInfo.currentTime);
+    if (playerRef.current) {
+      playerRef.current.seekTo(event.target.playerInfo.currentTime, true); // true for seconds
+    }
   };
 
   const opts: YouTubeProps["opts"] = {
