3de14fed6e47c3e3403203bbb56385df8f3eb6b3
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2024-10-07 00:23:21 +0530

    play pause functionality successfully added

--- app/websocket/page.tsx
+++ app/websocket/page.tsx
@@ -1,48 +1,62 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import YouTube, { YouTubeEvent, YouTubeProps } from "react-youtube";
 
+let socket: any = null;
+const yt = { current: null };
+
+// const YT = {
+//   current: {
+//     getPlayerState: Function,
+//     pauseVideo: Function,
+//   },
+// };
+
 const YouTubeFrame: React.FC = () => {
-  useEffect(() => {
-    // Create a new WebSocket connection to the server
-    const socket = new WebSocket("http://localhost:8080");
-
-    socket.onopen = () => {
-      console.log("WebSocket connection established");
-
-      // Now you can send data
-      const data = "true 2";
-      socket.send(data);
-    };
-
-    // When a message is received from the server
-    socket.onmessage = (event) => {
-      console.log(event.data);
-    };
-  }, []);
-
   const YT_LOADING = -1;
   const YT_BUFFERING = 3;
   const YT_PLAYING = 1;
   const YT_PAUSED = 2;
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [time, setTime] = useState(0);
+  const [data, setData] = useState("");
+  let dataVar;
+
+  useEffect(() => {
+    // Create a new WebSocket connection to the server
+    socket = new WebSocket("http://localhost:8080");
+
+    socket.onopen = () => {
+      console.log("WebSocket connection established");
+
+      // Now you can send data
+      // const data = "true 2";
+      // socket.send(data);
+    };
+
+    // When a message is received from the server
+    socket.onmessage = (event: any) => {
+      console.log(event.data);
+      setData(event.data);
+      dataVar = event.data;
+    };
+  }, []);
 
   // Event handler for when the video is ready
   const onVideoReady: YouTubeProps["onReady"] = (event) => {
     // access to player in all event handlers via event.target
     event.target.pauseVideo(); // Example: Start the video in paused mode
+
+    yt.current = event.target;
   };
 
   const frameCheck = (e: YouTubeEvent) => {
-    setIsPlaying(e.target.getPlayerState() == YT_PLAYING);
-    setTime(e.target.getCurrentTime());
-  };
-
-  const checkState = () => {
-    console.log(time, isPlaying);
+    socket.send(
+      `${
+        e.target.getPlayerState() == YT_PLAYING ||
+        e.target.getPlayerState() == YT_BUFFERING
+      } ${e.target.getCurrentTime()}`
+    );
   };
 
   // Options for the YouTube player (optional)
@@ -55,16 +69,29 @@ const YouTubeFrame: React.FC = () => {
     },
   };
 
+  if (data != dataVar) {
+    const parts = data.split(" ");
+    if (parts[0] == "true") {
+      //@ts-ignore
+      yt.current.playVideo();
+    } else if (parts[0] == "false") {
+      //@ts-ignore
+      yt.current.pauseVideo();
+    }
+  }
+
   return (
     <div>
       <h2>YouTube Video Frame</h2>
+
       <YouTube
         videoId="dQw4w9WgXcQ"
         opts={opts}
         onReady={onVideoReady}
         onStateChange={frameCheck}
       />
-      <button onClick={checkState}>check state</button>
+
+      <h2>{data}</h2>
     </div>
   );
 };
